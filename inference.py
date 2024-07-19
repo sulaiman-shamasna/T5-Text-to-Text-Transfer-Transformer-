506a73d8f4693ed6797f7984263f18c4b526a22a
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-07-19 20:21:30 +0200

    chore: :construction: Add inference script - Still under progress and needs improvements

--- inference.py
+++ inference.py
@@ -1,49 +1,38 @@
 import os
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
-def load_model_and_tokenizer(model_path, tokenizer_path):
-    model = T5ForConditionalGeneration.from_pretrained(model_path)
-    tokenizer = T5Tokenizer.from_pretrained(tokenizer_path)
-    return model, tokenizer
+class Inference:
+    def __init__(self, model_path, tokenizer_path):
+        self.model = T5ForConditionalGeneration.from_pretrained(model_path)
+        self.tokenizer = T5Tokenizer.from_pretrained(tokenizer_path)
 
-def do_correction(text, model, tokenizer):
-    input_text = f"assign tag: {text}"
-    inputs = tokenizer.encode(
-        input_text,
-        return_tensors='pt',
-        max_length=256,
-        padding='max_length',
-        truncation=True
-    )
+    def do_correction(self, text):
+        input_text = f"assign tag: {text}"
+        inputs = self.tokenizer.encode(
+            input_text,
+            return_tensors='pt',
+            max_length=256,
+            padding='max_length',
+            truncation=True
+        )
 
-    corrected_ids = model.generate(
-        inputs,
-        max_length=256,
-        num_beams=5,
-        early_stopping=True
-    )
+        corrected_ids = self.model.generate(
+            inputs,
+            max_length=256,
+            num_beams=5,
+            early_stopping=True
+        )
 
-    corrected_sentence = tokenizer.decode(
-        corrected_ids[0],
-        skip_special_tokens=True
-    )
-    return corrected_sentence
+        corrected_sentence = self.tokenizer.decode(
+            corrected_ids[0],
+            skip_special_tokens=True
+        )
+        return corrected_sentence
 
-def process_inference_data(data_dir, model, tokenizer):
-    for file in os.listdir(data_dir):
-        file_path = os.path.join(data_dir, file)
-        with open(file_path, 'r') as f:
-            sentence = f.read()
-            corrected_sentence = do_correction(sentence, model, tokenizer)
-            print(f"QUERY: {sentence}\nTAGS: {corrected_sentence}")
-
-def main():
-    model_path = 'results_t5small/checkpoint-9000/'
-    tokenizer_path = 'results_t5small'
-    data_dir = 'inference_data/'
-
-    model, tokenizer = load_model_and_tokenizer(model_path, tokenizer_path)
-    process_inference_data(data_dir, model, tokenizer)
-
-if __name__ == '__main__':
-    main()
+    def process_inference_data(self, data_dir):
+        for file in os.listdir(data_dir):
+            file_path = os.path.join(data_dir, file)
+            with open(file_path, 'r') as f:
+                sentence = f.read()
+                corrected_sentence = self.do_correction(sentence)
+                print(f"QUERY: {sentence}\nTAGS: {corrected_sentence}")
