437dfe79467be531e124d629cbe351cae43a26cb
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-07-19 20:20:51 +0200

    refactor: :recycle: Refactor the code - update main scrpt without touching the performance

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 from config import (
-    MODEL,
+    MODEL_NAME,
     BATCH_SIZE,
     NUM_PROCS,
     EPOCHS,
-    OUT_DIR,
+    OUTPUT_DIR,
     MAX_LENGTH,
     TRAIN_FILE,
     VALID_FILE,
-    LEARNING_RATE
+    LEARNING_RATE,
+    WARMUP_STEPS,
+    WEIGHT_DECAY,
+    LOGGING_STEPS,
+    SAVE_STEPS,
+    EVAL_STEPS,
+    SAVE_TOTAL_LIMIT,
+    DATALOADER_NUM_WORKERS
 )
-from data_processing import load_and_preprocess_data
-from model_setup import setup_model
-from training import train_model
+from data_processing import DataProcessor
+from model import ModelSetup
+from training import ModelTrainer
+from utils import save_tokenizer
 
 def main():
-    tokenized_train, tokenized_valid, tokenizer = load_and_preprocess_data(
-        TRAIN_FILE, VALID_FILE, MODEL, MAX_LENGTH, NUM_PROCS
+    data_processor = DataProcessor(MODEL_NAME, MAX_LENGTH, NUM_PROCS)
+    tokenized_train, tokenized_valid, tokenizer = data_processor.load_and_preprocess_data(TRAIN_FILE, VALID_FILE)
+
+    model_setup = ModelSetup(MODEL_NAME)
+    model, device = model_setup.setup_model()
+
+    trainer = ModelTrainer(
+        model, tokenized_train, tokenized_valid, OUTPUT_DIR, EPOCHS, BATCH_SIZE, LEARNING_RATE,
+        WARMUP_STEPS, WEIGHT_DECAY, LOGGING_STEPS, SAVE_STEPS, EVAL_STEPS, SAVE_TOTAL_LIMIT, DATALOADER_NUM_WORKERS
     )
+    trainer.train_model()
 
-    model, device = setup_model(MODEL)
-
-    trainer = train_model(
-        model, tokenized_train, tokenized_valid, OUT_DIR, EPOCHS, BATCH_SIZE, LEARNING_RATE
-    )
-
-    tokenizer.save_pretrained(OUT_DIR)
+    save_tokenizer(tokenizer, OUTPUT_DIR)
 
 if __name__ == '__main__':
     main()
